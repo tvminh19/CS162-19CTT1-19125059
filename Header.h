c5ace2c23dc4fbd41f32b0f0abfc4965884d6390
tvminh19 <56433333+tvminh19@users.noreply.github.com>
2020-04-19 14:43:13 +0700

    create something

---
+++ Header.h
@@ -0,0 +1,36 @@
+#ifndef function
+#define function
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+/* ----------------------------- Minh's funtions ---------------------------- */
+
+
+
+
+
+
+
+
+/* ------------------------------ Vu's funtions ----------------------------- */
+
+
+
+
+
+
+
+
+/* ----------------------------- Van's functions ---------------------------- */
+
+
+
+
+
+
+#endif //function
---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*This program look like moddle
+from M2V Team*/
+
+#include "Header.h"
+
+/* -------------------------- this is main function ------------------------- */
+int main(){
+    //code here 
+    return 0;
+}
+
+/* ---------------------- this is end of main function ---------------------- */
