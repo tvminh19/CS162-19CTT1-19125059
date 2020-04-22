3e17d10ed035ebfe532f0f04d0a236011a622683
tvminh19 <56433333+tvminh19@users.noreply.github.com>
2020-04-22 22:32:18 +0700

    Update minh.cpp

--- W00/minh.cpp
+++ W00/minh.cpp
@@ -1,8 +1,5 @@
 /* -------------------- this is Minh's file for functions ------------------- */
 #include "Header.h"
-
-//this is example
-int sum(int a, int b){
-    return a + b;
-    //trinh van minh 2020 - 2021
+int login(){
+    
 }
