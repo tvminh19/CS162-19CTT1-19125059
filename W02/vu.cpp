d89bbfed9facfba3f78890b234913b30c4001770
VinceTruong <truonghoangvu2001@gmail.com>
2020-05-05 23:22:56 +0700

    update Menu and Header.h

--- W02/vu.cpp
+++ W02/vu.cpp
@@ -1,7 +1,8 @@
 /* -------------------- this is Vu's file for functions ------------------- */
 #include "header.h"
 
-//W01
+//W01----------------------------------------------------------------------------------------------------
+
 void notCorrect()
 {
 	system("cls");
@@ -15,7 +16,7 @@ void notCorrect()
 void menuStudent()
 {
 	system("cls");
-	char c[5];
+	string s;
 	cout << "--------------- MENU ---------------" << endl;
 	cout << "0. Back" << endl;
 	cout << "1. Check-in." << endl;
@@ -23,8 +24,9 @@ void menuStudent()
 	cout << "3. View schedule." << endl;
 	cout << "4. View your scores of a coure." << endl << endl;
 	cout << "Input your choice (0-4) > ";
-	cin.ignore();
-	cin.get(c, 3, '\n');
+	cin >> s;
+	char* c = new char[s.length() + 1];
+	strcpy(c, s.c_str());
 
 	if (strcmp(c, "0") == 0) {
 		//Back
@@ -51,12 +53,14 @@ void menuStudent()
 		notCorrect();
 		menuStudent();
 	}
+	delete[] c;
 }
 
 void menuLecturer()
 {
 	system("cls");
-	char c[5];
+	string s;
+
 	cout << "----------------------- MENU -----------------------" << endl;
 	cout << "0. Back" << endl;
 	cout << "1. View list of courses in the current semester." << endl;
@@ -67,8 +71,9 @@ void menuLecturer()
 	cout << "6. Edit grade of a student." << endl;
 	cout << "7. View a scoreboard." << endl << endl;
 	cout << "Input your choice (0-7) > ";
-	cin.ignore();
-	cin.get(c, 3, '\n');
+	cin >> s;
+	char* c = new char[s.length() + 1];
+	strcpy(c, s.c_str());
 
 	if (strcmp(c, "0") == 0) {
 		//Back
@@ -107,6 +112,7 @@ void menuLecturer()
 		notCorrect();
 		menuLecturer();
 	}
+	delete[] c;
 }
 
 //-----------------------------------------------------------------------------------------
@@ -114,7 +120,8 @@ void menuLecturer()
 void Staff_class()
 {
 	system("cls");
-	char c[5];
+	string s;
+
 	cout << "------------- Academic Staff-class -------------" << endl;
 	cout << "0. Back" << endl;
 	cout << "1. Import students of a class from a csv file." << endl;
@@ -125,8 +132,9 @@ void Staff_class()
 	cout << "6. View list of classes." << endl;
 	cout << "7. View list of students in a class." << endl << endl;
 	cout << "Input your choice (0-7) > ";
-	cin.ignore();
-	cin.get(c, 3, '\n');
+	cin >> s;
+	char* c = new char[s.length() + 1];
+	strcpy(c, s.c_str());
 
 	if (strcmp(c, "0") == 0) {
 		//Back
@@ -165,12 +173,14 @@ void Staff_class()
 		notCorrect();
 		Staff_class();
 	}
+	delete[] c;
 }
 
 void Staff_course()
 {
 	system("cls");
-	char c[5];
+	string s;
+
 	cout << "---------------------------- Academic Staff-course ----------------------------" << endl;
 	cout << "0. Back" << endl;
 	cout << "1. Create / Update / Delete / view academic years (2019-2020) and semesters." << endl;
@@ -183,10 +193,11 @@ void Staff_course()
 	cout << "8. View list of courses in the current semester." << endl;
 	cout << "9. View list of students of a course." << endl;
 	cout << "10. View attendance list of a course." << endl;
-	cout << "11. Create / Update / Delete / View all lecturers." << endl << endl;
+	cout << "11. View all lecturers." << endl << endl;
 	cout << "Input your choice (0-11) > ";
-	cin.ignore();
-	cin.get(c, 3, '\n');
+	cin >> s;
+	char* c = new char[s.length() + 1];
+	strcpy(c, s.c_str());
 
 	if (strcmp(c, "0") == 0) {
 		//Back
@@ -195,6 +206,7 @@ void Staff_course()
 
 	else if (strcmp(c, "1") == 0) {
 		//3.1
+		Year_Semester();
 	}
 
 	else if (strcmp(c, "2") == 0) {
@@ -241,20 +253,22 @@ void Staff_course()
 		notCorrect();
 		Staff_course();
 	}
-
+	delete[] c;
 }
 
 void Staff_scoreboard()
 {
 	system("cls");
-	char c[5];
+	string s;
+
 	cout << "---------- Academic Staff-scoreboard ----------" << endl;
 	cout << "0. Back" << endl;
 	cout << "1. Search and view the scoreboard of a course" << endl;
 	cout << "2. Export a scoreboard to a csv file" << endl << endl;
 	cout << "Input your choice (0-2) > ";
-	cin.ignore();
-	cin.get(c, 3, '\n');
+	cin >> s;
+	char* c = new char[s.length() + 1];
+	strcpy(c, s.c_str());
 
 	if (strcmp(c, "0") == 0) {
 		//Back
@@ -273,19 +287,22 @@ void Staff_scoreboard()
 		notCorrect();
 		Staff_scoreboard();
 	}
+	delete[] c;
 }
 
 void Staff_attendanceList()
 {
 	system("cls");
-	char c[5];
+	string s;
+
 	cout << "---------- Academic Staff-attendance list ----------" << endl;
 	cout << "0. Back" << endl;
 	cout << "1. Search and view attendance list of a course." << endl;
 	cout << "2. Export a attendance list to a csv file." << endl;
 	cout << "Input your choice (0-2) > ";
-	cin.ignore();
-	cin.get(c, 3, '\n');
+	cin >> s;
+	char* c = new char[s.length() + 1];
+	strcpy(c, s.c_str());
 
 	if (strcmp(c, "0") == 0) {
 		//Back
@@ -304,12 +321,14 @@ void Staff_attendanceList()
 		notCorrect();
 		Staff_attendanceList();
 	}
+	delete[] c;
 }
 
 void menuStaffRole()
 {
 	system("cls");
-	char c[5];
+	string s;
+
 	cout << "--------------- MENU ---------------" << endl;
 	cout << "0. Back" << endl;
 	cout << "1. Academic staff-Class." << endl;
@@ -317,8 +336,9 @@ void menuStaffRole()
 	cout << "3. Academic staff-Scoreboard." << endl;
 	cout << "4. Academic staff-Attendance list." << endl << endl;
 	cout << "Input your choice (0-4) > ";
-	cin.ignore();
-	cin.get(c, 3, '\n');
+	cin >> s;
+	char* c = new char[s.length() + 1];
+	strcpy(c, s.c_str());
 
 	if (strcmp(c, "0") == 0) {
 		//Back
@@ -345,6 +365,7 @@ void menuStaffRole()
 		notCorrect();
 		menuStaffRole();
 	}
+	delete[] c;
 }
 
 void showMenu(int typeAccount)
@@ -370,17 +391,18 @@ void showMenu(int typeAccount)
 void displayAllroles(int typeAccount)
 {
 	system("cls");
-	char c[5];
+	string s;
 
 	//All roles
-	cout << "======== MENU ========\n";
+	cout << "======== MENU ========" << endl;
 	cout << "1. Show menu" << endl;
 	cout << "2. View profile info" << endl;
 	cout << "3. Change password" << endl;
 	cout << "4. Logout" << endl << endl;
 	cout << "Input your choice (1-4) > ";
-	cin.ignore();
-	cin.get(c, 3, '\n');
+	cin >> s;
+	char* c = new char[s.length() + 1];
+	strcpy(c, s.c_str());
 
 	//Show menu
 	if (strcmp(c, "1") == 0) {
@@ -410,4 +432,51 @@ void displayAllroles(int typeAccount)
 		notCorrect();
 		displayAllroles(typeAccount);
 	}
+	delete[] c;
+}
+
+//W02----------------------------------------------------------------------------------------------------
+
+void Year_Semester()
+{
+	system("cls");
+	string s;
+
+	cout << "-------------------- MENU --------------------" << endl;
+	cout << "0. Back" << endl;
+	cout << "1. Create an academic year and semesters" << endl;
+	cout << "2. Update an academic year and semesters" << endl;
+	cout << "3. Delete an academic year and semesters" << endl;
+	cout << "4. View an academic year and semesters" << endl << endl;
+	cout << "Input your choice (0-4) > ";
+	cin >> s;
+	char* c = new char[s.length() + 1];
+	strcpy(c, s.c_str());
+
+	if (strcmp(c, "0") == 0) {
+		//Back
+		Staff_course();
+	}
+
+	else if (strcmp(c, "1") == 0) {
+		//Create
+	}
+
+	else if (strcmp(c, "2") == 0) {
+		//Update
+	}
+
+	else if (strcmp(c, "3") == 0) {
+		//Delete
+	}
+
+	else if (strcmp(c, "4") == 0) {
+		//View
+	}
+
+	else {
+		notCorrect();
+		Year_Semester();
+	}
+	delete[] c;
 }
